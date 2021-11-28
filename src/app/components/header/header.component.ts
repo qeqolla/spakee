d430e524d3cb0384e61bbc20b7c04ef1feaa4875
Andrii Kmit <andrikmit241@gmail.com>
2021-11-28 17:27:13 +0200

    added components

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,48 @@
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit, AfterViewInit {
+
+  isInSystem = false
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+
+  // поміняти по можливості
+  toTop() {
+    // let scrollToTop = window.setInterval(() => {
+    //   let pos = window.pageYOffset;
+    //   if (pos > 0) {
+    //     window.scrollTo(0, pos - 50); // how far to scroll on each step
+    //   } else {
+    //     window.clearInterval(scrollToTop);
+    //   }
+    // }, 15);
+    let top = document.getElementById('top')
+    console.log(top)
+    if(top !== null) {
+      top.scrollIntoView();
+      console.log('qqq')
+    }
+  }
+
+  ngAfterViewInit() {
+    // let top = document.getElementById('top')
+    // if(top !== null) {
+    //   top.scrollIntoView();
+    // }
+  }
+
+
+  switch() {
+    this.isInSystem = !this.isInSystem
+  }
+}
---
+++ src/app/components/passgenpart/passgenpart.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-passgenpart',
+  templateUrl: './passgenpart.component.html',
+  styleUrls: ['./passgenpart.component.css']
+})
+export class PassgenpartComponent implements OnInit {
+  public Name!: string
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
