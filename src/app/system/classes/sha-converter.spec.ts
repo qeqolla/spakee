d99de88b1d1b06a14e12e86e83b26105807ad3cb
Andrii Kmit <andrikmit241@gmail.com>
2021-12-23 20:21:53 +0200

    added sha1 encryption

---
+++ src/app/system/classes/sha-converter.spec.ts
@@ -0,0 +1,7 @@
+import { ShaConverter } from './sha-converter';
+
+describe('ShaConverter', () => {
+  it('should create an instance', () => {
+    expect(new ShaConverter()).toBeTruthy();
+  });
+});
